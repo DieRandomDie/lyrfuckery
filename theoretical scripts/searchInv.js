0ba73b30937fc91ca9184ff213c2c3d052653ea9
Eric Salazar <67451306+DieRandomDie@users.noreply.github.com>
2024-04-12 10:00:44 -0500

    finished

--- theoretical scripts/searchInv.js
+++ theoretical scripts/searchInv.js
@@ -1,27 +1,24 @@
-// removes case-sensitivity from jQuery's contains
+// add text input for filter
+$('<input type="text" oninput="filterItems(this.value)">').insertBefore($('#limit').next())
+
+// overrides the existing client code for the check-all box in inventory
+$(document).on("change", ".select_all", function(e) {
+    $('.unequippedItems tbody tr td input:hidden').prop("checked", false);
+})
+// stack overflow solution to remove case sensitivity from jquery's "contains"
 jQuery.expr[':'].icontains = function(a, i, m) {
   return jQuery(a).text().toUpperCase()
       .indexOf(m[3].toUpperCase()) >= 0
 }
-function searchInv(j) {
+
+// jank solutions will be jank
+function filterItems(j) {
+    // show hidden stuff
+    $('.unequippedItems tbody tr').show()
     $('.unequippedItems tbody tr').each(function(i) {
         if(i>3){
+            // hide anything that doesn't match input text
             $(this).not(`:has(td:icontains("${j}"))`).hide()
         }
       })
 }
-// item search with optional auto-check box ticking. This only works from top to bottom so non-tradeables will be skipped and the final checked amount will be qty minus non-tradeables.
-/*function searchInv(j, qty) {
-    let jewel = $(`tr:has(td:icontains("${j}"))`)
-    $('.unequippedItems tbody tr').each(function(i) {
-        if(i>3){
-            $(this).not(`:has(td:icontains("${j}"))`).hide()
-        }
-      })
-    if(qty) {
-        if (!jewel.length > qty) { qty = jewel.length }
-        for (i=0;i<qty;i++) {
-            jewel[i].children[0].children[0].checked = true
-        }
-    }
-}/*
